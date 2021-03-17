199b1d684640137b7f4073d74f2c3c1e2dd3998c
Nathan Wright <nathan@brandalism.com>
2021-03-17 12:31:13 -0500

    making the shadow root reactive

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { observable, observe } from "@nx-js/observer-util";
+import { observable, observe, raw } from "@nx-js/observer-util";
 import { render, html, svg } from "uhtml";
 import props from "element-props";
 
@@ -13,12 +13,21 @@ function define(tagName, componentObj, options = {}) {
   const { mixins = [], base = HTMLElement, extend = undefined } = options;
   const prototypeChain = Array.isArray(mixins) ? mixins : [mixins];
   prototypeChain.push(componentObj);
+
   class DMMTElement extends base {
+    static get attrs() {
+      return {
+        disabled: Boolean,
+        hidden: Boolean,
+      };
+    }
     static get observedAttributes() {
       return [
         /* array of attribute names to monitor for changes */
       ];
     }
+
+    shadowDOM = true;
 
     constructor() {
       super();
@@ -31,19 +40,44 @@ function define(tagName, componentObj, options = {}) {
         obj: Object,
         fooBar: String,
       };
-      this.state = this.props = observable(props(this, obsAttr));
+      // this.state = this.props = observable(props(this, obsAttr));
+      this.state = observable({});
+      this.internalState = observable({ shadowDOM: this.shadowDOM });
+      // this.props = props(this, obsAttr);
+      // this.props = props(this, {
+      //   n: Number,
+      //   b: Boolean,
+      //   o: Object,
+      //   a: Array,
+      //   s: String,
+      //   d: Date,
+      // });
+
+      this.props = new Proxy(this.attributes, {
+        get(target, name) {
+          debugger;
+          const attr = target[name];
+          return attr && attr.value;
+        },
+      });
 
       observe(() => {
         console.log("THe state of foo:", this.state.foo);
         // debugger;
       });
 
-      const rootNode = DMMTElement.prototype.shadowDOM
-        ? this.attachShadow({ mode: "open" })
-        : this.renderRoot || this;
+      // this.rootNode = this.shadowDOM
+      //   ? this.attachShadow({ mode: "open" })
+      //   : this.renderRoot || this;
 
       observe(() => {
-        render(rootNode, this.render());
+        this.rootNode = this.internalState.shadowDOM
+          ? this.shadowRoot
+            ? this.shadowRoot
+            : this.attachShadow({ mode: "open" })
+          : this.renderRoot || this;
+        console.log("ROOT NODE", this.rootNode);
+        render(this.rootNode, this.render());
       });
     }
 
@@ -74,11 +108,13 @@ function define(tagName, componentObj, options = {}) {
       console.log("DMMTElement testBlah");
     }
   }
-  Object.defineProperty(DMMTElement.prototype, "shadowDOM", {
-    value: true,
-    writable: true,
-    enumerable: false,
-  });
+  // Object.defineProperties(DMMTElement.prototype, {
+  //   shadowDOM: {
+  //     value: true,
+  //     writable: true,
+  //     enumerable: false,
+  //   },
+  // });
 
   prototypeChain.forEach((p) => {
     Object.entries(p).forEach(([key, value]) => {
@@ -117,6 +153,7 @@ const Foo = {
   render() {
     return html`<h1>
       Hello 👋 µhtml : ${this.state.foo} : ${this.state.xxx}
+      <slot></slot>
     </h1>`;
   },
 };
@@ -137,3 +174,13 @@ const Bar = {
   },
 };
 define("foo-tag", Foo, { mixins: [Bar] });
+
+// TODO: Observable root node (switch between light and shadow dom rendering to capture styles!)
+// TODO: Wire up observed attributes
+// TODO: Styles
+// TODO: Limit reflection to defined attributes
+// TODO: Make attributes have "-" instead of no-dash
+// TODO: ABility to turn off shadow dom
+// TODO: Add global styles
+// TODO: attribute accessors for array/objects
+// TODO: Rename library to ZenElement? BalancedElement
