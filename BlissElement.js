f1594e92b53b475c36bcbffdfd1bd76c54da16bf
Nathan Wright <nathan@brandalism.com>
2021-03-18 00:07:05 -0500

    wip

---
+++ BlissElement.js
@@ -0,0 +1,92 @@
+import { observable, observe } from "@nx-js/observer-util";
+import { render, html, svg } from "uhtml";
+import props from "element-props";
+
+const lifecycleMethods = {
+  connectedCallback: true,
+  disconnectedCallback: true,
+  adoptedCallback: true,
+  attributeChangedCallback: true,
+};
+
+function define(tagName, componentObj, options = {}) {
+  const { mixins = [], base = HTMLElement, extend = undefined } = options;
+  const prototypeChain = Array.isArray(mixins) ? mixins : [mixins];
+  prototypeChain.push(componentObj);
+  class BlissElement extends base {
+    static get observedAttributes() {
+      return [
+        /* array of attribute names to monitor for changes */
+      ];
+    }
+
+    constructor() {
+      super();
+
+      const obsAttr = {
+        foo: String,
+        num: Number,
+        bool: Boolean,
+        arr: Array,
+        obj: Object,
+        fooBar: String,
+      };
+      this.state = this.props = observable(props(this, obsAttr));
+
+      observe(() => {
+        console.log("THe state of foo:", this.state.foo);
+        // debugger;
+      });
+
+      const rootNode =
+        this.hasShadowRoot === false
+          ? this
+          : this.attachShadow({ mode: "open" });
+
+      observe(() => {
+        render(rootNode, this.render());
+      });
+    }
+
+    connectedCallback() {
+      if (super.connectedCallback) super.connectedCallback();
+    }
+
+    disconnectedCallback() {
+      if (super.disconnectedCallback) super.disconnectedCallback();
+    }
+
+    adoptedCallback() {
+      if (super.adoptedCallback) super.adoptedCallback();
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+      if (super.attributeChangedCallback) super.attributeChangedCallback();
+    }
+
+    render() {
+      return html``;
+    }
+  }
+
+  prototypeChain.forEach((p) => {
+    Object.entries(p).forEach(([key, value]) => {
+      // If property is a lifecycleMethod, then call the original function and our new function. Behaves like `super.myMethod()`.
+      if (typeof value === typeof Function && lifecycleMethods[value.name]) {
+        const originalFn = BlissElement.prototype[key];
+        BlissElement.prototype[key] = function (args) {
+          originalFn.call(this, args);
+          value.call(this, args);
+        };
+        return;
+      }
+
+      // If not a lifecycleMethod then overwrite existing property.
+      BlissElement.prototype[key] = value;
+    });
+  });
+
+  customElements.define(tagName, BlissElement, { extends: extend });
+}
+
+export { define, html, svg, observable, observe };
